442f3925b08dcd33f184f779e6b02e9ef0915b76
tsunyoku <mbruhyo@gmail.com>
2022-09-17 23:07:47 +0100

    chore: run formatter

--- surrealdb/__init__.py
+++ surrealdb/__init__.py
@@ -1,5 +1,5 @@
-from .clients.sync import SurrealDBClient
 from .clients.asynchronous import AsyncSurrealDBClient
+from .clients.sync import SurrealDBClient
 
 __version__ = "0.0.1"
 __author__ = "tsunyoku"
