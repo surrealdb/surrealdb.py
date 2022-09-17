7f531e946b7fd1f5f7c8a89794a6ae8519596803
tsunyoku <mbruhyo@gmail.com>
2022-09-17 23:38:54 +0100

    chore: remove author dunder attribute and add copyright

--- surrealdb/__init__.py
+++ surrealdb/__init__.py
@@ -2,7 +2,7 @@ from .clients import AsyncSurrealDBClient
 from .clients import SurrealDBClient
 
 __version__ = "0.0.1"
-__author__ = "tsunyoku"
 __title__ = "surrealdb"
 __license__ = "Apache-2.0"
+__copyright__ = "SurrealDB Ltd"
 __all__ = ("SurrealDBClient", "AsyncSurrealDBClient",)
