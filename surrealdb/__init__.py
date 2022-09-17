f0bccd6045385e6eb780c9a14ed59aa108d9029c
tsunyoku <mbruhyo@gmail.com>
2022-09-17 23:37:42 +0100

    feat: add extra dunder module attributes

--- surrealdb/__init__.py
+++ surrealdb/__init__.py
@@ -3,4 +3,6 @@ from .clients import SurrealDBClient
 
 __version__ = "0.0.1"
 __author__ = "tsunyoku"
+__title__ = "surrealdb"
+__license__ = "Apache-2.0"
 __all__ = ("SurrealDBClient", "AsyncSurrealDBClient",)
