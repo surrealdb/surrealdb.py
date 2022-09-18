920133762ad963f7753c8feb0fc46eeeb8b021f8
James <tsunyoku@gmail.com>
2022-09-18 17:09:09 +0100

    Add websocket implementation (#6)
    * feat: add pre-commit config
    
    * feat: add dev requirements
    
    * feat: implement sync & async clients
    
    * chore: run formatter
    
    * chore: rename sync client for consistency
    
    * chore: add `__all__` to all files
    
    * chore: refine imports
    
    * feat: improve compatibility for lower py versions
    
    * chore: run formatter
    
    * feat: change `__all__` to use tuples instead of lists
    
    * feat: add extra dunder module attributes
    
    * chore: remove author from setup.py
    
    * chore: remove author dunder attribute and add copyright
    
    * chore: add copyright headers to files
    
    * feat: switch to black
    
    * feat: add license to setup.py
    
    * chore: run formatter
    
    * feat: add more pre-commit hooks
    
    * chore: run formatter
    
    * fix: replace autopep8 with black in dev requirements
    
    * feat: add pyproject.toml file for poetry
    
    * chore: change versioning
    
    * chore: remove unused `requests` dependency
    
    * fix: change `method` type annotation to `str` to support 3.7
    
    * fix: more changes to complete py3.7 support
    
    * feat: add author
    
    * fix: `orjson` in requirements.txt
    
    * feat: move exceptions from folder into single file
    
    * fix: incorrect exception import
    
    * feat: rename `SurrealDBClient` to `SyncSurrealDBClient`
    
    * feat: remove sync client, move & rename async client
    
    * chore: remove un-used requirements file
    
    * force `namespace`, `database`, `username` and `password` to be keyword arguments
    
    * chore: delete setup.py inplace of pyproject.toml
    
    * fix: add `Optional`s on `__aexit__`
    
    * chore: add .vscode to gitignore
    
    * chore: move client.py to clients/http.py
    
    * chore: remove unused dev requirements
    
    * feat: add ws requirements
    
    * feat: add base ws client, increment library version
    
    * feat: add TypedDict for ws request
    
    * feat: add response listening, add remaining request types
    
    * feat: force args after `url` to be keyword
    
    * feat: add username/password auth from ctor
    
    * fix: bugs on authentication
    
    * feat: add ping
    
    * feat: improve function typing
    
    * feat: further improve typing
    
    * fix: remove old client
    
    * Update surrealdb/clients/ws.py
    
    Co-authored-by: Caeden <caedenperelliharris@gmail.com>
    
    * Update surrealdb/clients/ws.py
    
    Co-authored-by: Caeden <caedenperelliharris@gmail.com>
    
    * fix: list type annotation
    
    * feat: remove unused import and add future annotations
    
    * chore: rename `SurrealDBWSClient` to `SurrealDBWebsocketClient`
    
    * chore: remove `SurrealDB` suffix from client names
    
    * fix: port https://github.com/surrealdb/surrealdb.py/pull/7 to http client
    
    Co-authored-by: tsunyoku <mbruhyo@gmail.com>
    Co-authored-by: Caeden <caedenperelliharris@gmail.com>

--- surrealdb/common/exceptions.py
+++ surrealdb/common/exceptions.py
@@ -13,8 +13,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-__all__ = ("SurrealException",)
+__all__ = ("SurrealException", "SurrealWebsocketException")
 
 
 class SurrealException(Exception):
+    pass
+
+
+class SurrealWebsocketException(SurrealException):
     pass
