02edf3129d148943df381bc868057ae1e2fd04eb
Grzegorz Koperwas <admin@grzegorzkoperwas.site>
2022-10-17 23:06:25 +0200

    Add support for creation of record links

--- surrealdb/common/json.py
+++ surrealdb/common/json.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from typing import Any
+from typing import Any, Iterable
 from typing import Union
 
 
@@ -28,13 +28,21 @@ __all__ = (
 )
 
 
-def dumps(obj: Any, **kwargs: Any) -> str:
-    """Serialize an object to a JSON string."""
-    result = jsonlib.dumps(obj, **kwargs)
-    if isinstance(result, bytes):
-        result = result.decode("utf-8")
-
-    return result
+def dumps(obj: Any) -> str:
+    """Serialize an object to a SurrealDB string."""
+    if any(isinstance(obj, t) for t in [int, float]):
+        return repr(obj)
+    elif isinstance(obj, str):
+        return f'"{obj}"'
+    elif hasattr(obj, "id"):
+        return obj.id
+    elif isinstance(obj, dict):
+        if obj.get("id") is not None:
+            return obj["id"]
+        return "{" + ", ".join(f"{k}: {dumps(v)}" for k, v in obj.items()) + "}"
+    elif isinstance(obj, Iterable):
+        return f'[{", ".join(dumps(v) for v in obj)}]'
+    raise Exception(f"Object is not json serializable: {repr(obj)}")
 
 
 def loads(content: Union[bytes, str], **kwargs: Any) -> Any:
