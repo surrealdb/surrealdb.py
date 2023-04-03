7537ff84e79a67da468d23e7fecfb397bfac043a
Caeden Perelli-Harris <caedenperelliharris@gmail.com>
2023-04-03 18:06:17 +0100

    ci: Create a workflow that runs ruff (#52)

--- surrealdb/__init__.py
+++ surrealdb/__init__.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from .ws import Surreal
 from .http import SurrealHTTP
+from .ws import Surreal
 
 __all__ = ("Surreal", "SurrealHTTP")
