cc0c190f176832a717aacb8442b78acc51997c59
maxwellflitton <maxwellflitton@gmail.com>
2023-07-12 22:32:00 +0100

    updating whl names

--- setup.py
+++ setup.py
@@ -4,29 +4,30 @@ from setuptools_rust import Binding, RustExtension
 
 import os
 import platform
-import subprocess
+from subprocess import Popen, PIPE, check_output, CalledProcessError
+
 
 def set_libclang_path(libclang_path):
     if platform.system() == "Windows":
         os.environ["LIBCLANG_PATH"] = libclang_path
     else:
-        outcome = subprocess.run(["export", f"LIBCLANG_PATH={libclang_path}"], shell=True)
+        outcome = Popen(["export", f"LIBCLANG_PATH={libclang_path}"], shell=True)
         outcome.wait()
 
 def install_libclang_mac():
-    outcome = subprocess.run(["brew", "install", "llvm"], shell=True)
+    outcome = Popen(["brew", "install", "llvm"], shell=True)
     outcome.wait()
 
 def install_libclang_linux():
     distro = platform.linux_distribution()[0].lower()
     if distro in ["ubuntu", "debian"]:
-        outcome = subprocess.run(["sudo", "apt-get", "install", "libclang-dev"], shell=True)
+        outcome = Popen(["sudo", "apt-get", "install", "libclang-dev"], shell=True)
         outcome.wait()
     elif distro == "fedora":
-        outcome = subprocess.run(["sudo", "dnf", "install", "libclang-devel"], shell=True)
+        outcome = Popen(["sudo", "dnf", "install", "libclang-devel"], shell=True)
         outcome.wait()
     elif distro == "arch":
-        outcome = subprocess.run(["sudo", "pacman", "-S", "clang"], shell=True)
+        outcome = Popen(["sudo", "pacman", "-S", "clang"], shell=True)
         outcome.wait()
     else:
         print("Unsupported Linux distribution. Please install libclang manually.")
@@ -35,22 +36,26 @@ def solve_libclang_error():
     libclang_path = ""
     
     if platform.system() == "Darwin":
-        try:
-            libclang_path = subprocess.check_output(["brew", "--prefix", "llvm"], shell=True).decode().strip() + "/lib/libclang.dylib"
-        except subprocess.CalledProcessError:
-            install_libclang_mac()
-            libclang_path = "/usr/local/opt/llvm/lib/libclang.dylib"
+        install_libclang_mac()
+        libclang_path = "/usr/local/opt/llvm/lib/libclang.dylib"
+        # try:
+        #     libclang_path = check_output(["brew", "--prefix", "llvm"], shell=True).decode().strip() + "/lib/libclang.dylib"
+        # except CalledProcessError:
+        #     install_libclang_mac()
+        #     libclang_path = "/usr/local/opt/llvm/lib/libclang.dylib"
     elif platform.system() == "Windows":
         try:
-            libclang_path = subprocess.check_output(["where", "libclang.dll"]).decode().strip()
-        except subprocess.CalledProcessError:
+            libclang_path = check_output(["where", "libclang.dll"]).decode().strip()
+        except CalledProcessError:
             print("Libclang not found. Please install LLVM and set LIBCLANG_PATH manually.")
     elif platform.system() == "Linux":
-        try:
-            libclang_path = subprocess.check_output(["find", "/usr", "-name", "libclang.so"]).decode().strip()
-        except subprocess.CalledProcessError:
-            install_libclang_linux()
-            libclang_path = "/usr/lib/libclang.so"
+        install_libclang_linux()
+        libclang_path = "/usr/lib/libclang.so"
+        # try:
+        #     libclang_path = check_output(["find", "/usr", "-name", "libclang.so"]).decode().strip()
+        # except CalledProcessError:
+        #     install_libclang_linux()
+        #     libclang_path = "/usr/lib/libclang.so"
     else:
         print("Unsupported operating system.")
     
