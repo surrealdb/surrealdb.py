f2e5030bbd8e94b582ab081d7d4c14a999e30589
maxwellflitton <maxwellflitton@gmail.com>
2023-07-14 21:28:58 +0100

    adding llvm to rocksdb

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ import subprocess
 
 print("Updating apt-get...")
 # Update apt-get
-update_apt = subprocess.Popen(["sudo", "apt-get", "update"])
+update_apt = subprocess.Popen(["apt-get", "update"])
 update_apt.wait()
 print("Updating apt-get... Done")
 
 print("Installing llvm...")
 # Install LLVM using apt-get
-install_llvm = subprocess.Popen(["sudo", "apt-get", "install", "-y", "llvm-dev"])
+install_llvm = subprocess.Popen(["apt-get", "install", "-y", "llvm-dev"])
 install_llvm.wait()
 print("Installing llvm... Done")
 
