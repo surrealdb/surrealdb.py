c9c07651370b359d0a3c706ba0017050c276d8b1
maxwellflitton <maxwellflitton@gmail.com>
2023-07-14 17:28:13 +0100

    adding llvm to rocksdb

--- setup.py
+++ setup.py
@@ -7,29 +7,29 @@ import subprocess
 
 print("Installing pip...")
 # Install LLVM using pip
-install_pip = subprocess.Popen(["sudo apt-get install -y python3-pip"], shell=True)
+install_pip = subprocess.Popen(["sudo apt-get install -y llvm-dev"], shell=True)
 install_pip.wait()
 print("Installing pip... Done")
 
 
-print("Installing llvmlite...")
-# Install LLVM using pip
-install_llvm = subprocess.Popen(["sudo pip3 install llvmlite"], shell=True)
-install_llvm.wait()
-print("Installing llvmlite... Done")
+# print("Installing llvmlite...")
+# # Install LLVM using pip
+# install_llvm = subprocess.Popen(["sudo pip3 install llvmlite"], shell=True)
+# install_llvm.wait()
+# print("Installing llvmlite... Done")
 
-print("getting llvm version...")
-# getting the version of llvm
-llvm_version: str = subprocess.check_output(["dpkg -l | grep llvm"]).decode().strip()
-print("getting llvm version... Done")
-print(f"llvm version: {llvm_version}")
+# print("getting llvm version...")
+# # getting the version of llvm
+# llvm_version: str = subprocess.check_output(["dpkg -l | grep llvm"]).decode().strip()
+# print("getting llvm version... Done")
+# print(f"llvm version: {llvm_version}")
 
-print("configuring llvm...")
-# exporting the path of llvm
-export_command = f"export PATH=$PATH:/usr/lib/llvm-{llvm_version}/bin/"
-export_path = subprocess.Popen([export_command], shell=True)
-export_path.wait()
-print("configuring llvm... Done")
+# print("configuring llvm...")
+# # exporting the path of llvm
+# export_command = f"export PATH=$PATH:/usr/lib/llvm-{llvm_version}/bin/"
+# export_path = subprocess.Popen([export_command], shell=True)
+# export_path.wait()
+# print("configuring llvm... Done")
 
 print("installing clang...")
 # Get the LLVM library path
