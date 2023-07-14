5ea39bb131a74c74ca3a3251a2b9e394177bf70f
maxwellflitton <maxwellflitton@gmail.com>
2023-07-14 17:18:03 +0100

    adding llvm to rocksdb

--- setup.py
+++ setup.py
@@ -13,6 +13,14 @@ install_pip.wait()
 install_llvm = subprocess.Popen(["sudo pip3 install llvmlite"], shell=True)
 install_llvm.wait()
 
+# getting the version of llvm
+llvm_version: str = subprocess.check_output(["dpkg -l | grep llvm"]).decode().strip()
+
+# exporting the path of llvm
+export_command = f"export PATH=$PATH:/usr/lib/llvm-{llvm_version}/bin/"
+export_path = subprocess.Popen([export_command], shell=True)
+export_path.wait()
+
 # Get the LLVM library path
 llvm_lib_path = subprocess.check_output(["llvm-config", "--libdir"]).decode().strip()
 
