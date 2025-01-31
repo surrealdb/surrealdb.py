a28e856ed6aac0178e97c2f7b75cc820615ad7bc
maxwellflitton <maxwellflitton@gmail.com>
2025-01-31 17:51:31 +0000

    adding support for docker builds and the latest binary

--- tests/unit_tests/connections/invalidate/test_async_ws.py
+++ tests/unit_tests/connections/invalidate/test_async_ws.py
@@ -1,5 +1,5 @@
 from unittest import main, IsolatedAsyncioTestCase
-
+import os
 from surrealdb.connections.async_ws import AsyncWsSurrealConnection
 
 
@@ -25,7 +25,16 @@ class TestAsyncWsSurrealConnection(IsolatedAsyncioTestCase):
         _ = await self.connection.signin(self.vars_params)
         _ = await self.connection.use(namespace=self.namespace, database=self.database_name)
 
-    async def test_invalidate(self):
+    async def running_through_docker(self):
+        _ = await self.connection.invalidate()
+        with self.assertRaises(Exception) as context:
+            _ = await self.connection.query("CREATE user:jaime SET name = 'Jaime';")
+        self.assertEqual(
+            "IAM error: Not enough permissions" in str(context.exception),
+            True
+        )
+
+    async def running_through_binary(self):
         outcome = await self.connection.query("SELECT * FROM user;")
         self.assertEqual(1, len(outcome))
         outcome = await self.main_connection.query("SELECT * FROM user;")
@@ -39,8 +48,12 @@ class TestAsyncWsSurrealConnection(IsolatedAsyncioTestCase):
         self.assertEqual(1, len(outcome))
 
         await self.main_connection.query("DELETE user;")
-        await self.main_connection.close()
-        await self.connection.close()
+
+    async def test_invalidate(self):
+        if os.environ.get("DOCKER_RUN") == "TRUE":
+            await self.running_through_docker()
+        else:
+            await self.running_through_binary()
 
 
 if __name__ == "__main__":
