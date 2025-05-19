9622672f080a56fb466270b49ae8421ee8b0cbc7
Yusuke Kuoka <ykuoka@gmail.com>
2025-05-19 23:31:20 +0900

    Fix None encoding and decoding for SurrealDB v2.2.x and later (#184)

--- tests/unit_tests/data_types/test_none.py
+++ tests/unit_tests/data_types/test_none.py
@@ -6,6 +6,7 @@ Notes:
     will have to look into schema objects for more complete serialization.
 """
 from unittest import main, IsolatedAsyncioTestCase
+from os import environ
 
 from surrealdb.connections.async_ws import AsyncWsSurrealConnection
 from surrealdb.data.types.record_id import RecordID
@@ -30,7 +31,7 @@ class TestAsyncWsSurrealConnectionNone(IsolatedAsyncioTestCase):
         await self.connection.use(namespace=self.namespace, database=self.database_name)
 
         # Cleanup
-        await self.connection.query("DELETE person;")
+        await self.connection.query("REMOVE TABLE person;")
 
     async def test_none(self):
         schema = """
@@ -56,6 +57,46 @@ class TestAsyncWsSurrealConnectionNone(IsolatedAsyncioTestCase):
         self.assertEqual(record_check, outcome["id"])
         self.assertEqual("Dave", outcome["name"])
         self.assertEqual(34, outcome["age"])
+
+        outcome = await self.connection.query("SELECT * FROM person")
+        self.assertEqual(2, len(outcome))
+
+        await self.connection.query("REMOVE TABLE person;")
+        await self.connection.close()
+
+    async def test_none_with_query(self):
+        is_sdb21 = environ.get("SURREALDB_VERSION", "v2.1.4").startswith("v2.1.")
+        schema = """
+            DEFINE TABLE person SCHEMAFULL TYPE NORMAL;
+            DEFINE FIELD name ON person TYPE string;
+            DEFINE FIELD nums ON person TYPE array<option<int>>;
+        """
+        await self.connection.query(schema)
+        outcome = await self.connection.query(
+            "UPSERT person MERGE {id: $id, name: $name, nums: $nums}",
+            {"id": [1,2], "name": "John", "nums": [None]}
+        )
+        record_check = RecordID(table_name="person", identifier=[1, 2])
+        self.assertEqual(1, len(outcome))
+        self.assertEqual(record_check, outcome[0]["id"])
+        self.assertEqual("John", outcome[0]["name"])
+        if is_sdb21:
+            self.assertEqual([], outcome[0].get("nums"))
+        else:
+            self.assertEqual([None], outcome[0].get("nums"))
+
+        outcome = await self.connection.query(
+            "UPSERT person MERGE {id: $id, name: $name, nums: $nums}",
+            {"id": [3,4], "name": "Dave", "nums": [None]}
+        )
+        record_check = RecordID(table_name="person", identifier=[3, 4])
+        self.assertEqual(1, len(outcome))
+        self.assertEqual(record_check, outcome[0]["id"])
+        self.assertEqual("Dave", outcome[0]["name"])
+        if is_sdb21:
+            self.assertEqual([], outcome[0].get("nums"))
+        else:
+            self.assertEqual([None], outcome[0].get("nums"))
 
         outcome = await self.connection.query("SELECT * FROM person")
         self.assertEqual(2, len(outcome))
