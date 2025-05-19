4a2987c2c4005254fc433c1af54a9ac4f681bd45
Yuske Kuoka <yusuke.kuoka@surrealdb.com>
2025-05-19 12:17:39 +0000

    Add failing test

--- tests/unit_tests/data_types/test_none.py
+++ tests/unit_tests/data_types/test_none.py
@@ -30,7 +30,7 @@ class TestAsyncWsSurrealConnectionNone(IsolatedAsyncioTestCase):
         await self.connection.use(namespace=self.namespace, database=self.database_name)
 
         # Cleanup
-        await self.connection.query("DELETE person;")
+        await self.connection.query("REMOVE TABLE person;")
 
     async def test_none(self):
         schema = """
@@ -56,6 +56,39 @@ class TestAsyncWsSurrealConnectionNone(IsolatedAsyncioTestCase):
         self.assertEqual(record_check, outcome["id"])
         self.assertEqual("Dave", outcome["name"])
         self.assertEqual(34, outcome["age"])
+
+        outcome = await self.connection.query("SELECT * FROM person")
+        self.assertEqual(2, len(outcome))
+
+        await self.connection.query("REMOVE TABLE person;")
+        await self.connection.close()
+
+    async def test_none_with_query(self):
+        schema = """
+            DEFINE TABLE person SCHEMAFULL TYPE NORMAL;
+            DEFINE FIELD name ON person TYPE string;
+            DEFINE FIELD nums ON person TYPE array<option<int>>;
+        """
+        await self.connection.query(schema)
+        outcome = await self.connection.query(
+            "UPSERT person MERGE {id: $id, name: $name, nums: $nums}",
+            {"id": [1,2], "name": "John", "nums": [None]}
+        )
+        record_check = RecordID(table_name="person", identifier=[1, 2])
+        self.assertEqual(1, len(outcome))
+        self.assertEqual(record_check, outcome[0]["id"])
+        self.assertEqual("John", outcome[0]["name"])
+        self.assertEqual([None], outcome[0].get("nums"))
+
+        outcome = await self.connection.query(
+            "UPSERT person MERGE {id: $id, name: $name, nums: $nums}",
+            {"id": [3,4], "name": "Dave", "nums": [None]}
+        )
+        record_check = RecordID(table_name="person", identifier=[3, 4])
+        self.assertEqual(1, len(outcome))
+        self.assertEqual(record_check, outcome[0]["id"])
+        self.assertEqual("Dave", outcome[0]["name"])
+        self.assertEqual([None], outcome[0].get("nums"))
 
         outcome = await self.connection.query("SELECT * FROM person")
         self.assertEqual(2, len(outcome))
