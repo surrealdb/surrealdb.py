acb1a6371b7613c4f57988fa5284e56c51fbc603
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-10-24 11:58:12 +0100

    Remove warnings

--- src/surrealdb/cbor/decoder.py
+++ src/surrealdb/cbor/decoder.py
@@ -3,7 +3,3 @@ from warnings import warn
 from surrealdb.cbor._decoder import CBORDecoder as CBORDecoder
 from surrealdb.cbor._decoder import load as load
 from surrealdb.cbor._decoder import loads as loads
-
-warn(
-    "The cbor.decoder module has been deprecated. Instead import everything directly from cbor."
-)
--- src/surrealdb/cbor/encoder.py
+++ src/surrealdb/cbor/encoder.py
@@ -4,7 +4,3 @@ from surrealdb.cbor._encoder import CBOREncoder as CBOREncoder
 from surrealdb.cbor._encoder import dump as dump
 from surrealdb.cbor._encoder import dumps as dumps
 from surrealdb.cbor._encoder import shareable_encoder as shareable_encoder
-
-warn(
-    "The cbor.encoder module has been deprecated. Instead import everything directly from cbor."
-)
