466a985f97ae94ce506d3b12a998029baf4a249a
Ryan Zavislak <ryan.zavislak@gmail.com>
2026-02-16 18:01:09 -0800

    ruff fix SIM114

--- src/surrealdb/data/utils.py
+++ src/surrealdb/data/utils.py
@@ -7,9 +7,7 @@ from surrealdb.data.types.table import Table
 
 
 def process_record(record: RecordIdType) -> RecordID | Table:
-    if isinstance(record, RecordID):
-        return record
-    elif isinstance(record, Table):
+    if isinstance(record, RecordID) or isinstance(record, Table):
         return record
     else:
         if ":" in record:
